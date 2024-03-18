b9fd6200fbe5e1d39ead8fd30990de2d1afe37e9
Hayato-T-08 <104160005+Hayato-T-08@users.noreply.github.com>
2024-03-18 18:57:22 +0900

    de

--- tessoku/b65_bfs.cpp
+++ tessoku/b65_bfs.cpp
@@ -12,11 +12,52 @@ using mint = modint1000000007;
 const int inf = 1e9;
 const int mod = 1e9+7;
 const ll linf = 1e18;
-
+vector<int> dist;
+Graph G;
+queue<int> que;
+void bfs(int pos){
+    que.push(pos);
+    dist[pos] = 0;
+    while(!que.empty()){
+        int cur = que.front();
+        que.pop();
+        for(auto next : G[cur]){
+            if(dist[next] != -1) continue;
+            dist[next] = dist[cur] + 1;
+            que.push(next);
+        }
+    }
+}
 int main() {
-    int n;
-    cin >> n;
-    int ans = 0;
-    cout << ans << endl;
+    int n,t;
+    cin>>n>>t;
+    G.resize(n+1);
+    dist.assign(n+1, -1);
+    vector<P> List;
+    rep1(i,n-1){
+        int a,b;
+        cin>>a>>b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    bfs(t);
+
+
+    rep1(i,n){
+        List.push_back({dist[i], i});
+    }
+    sort(List.begin(), List.end());
+    reverse(List.begin(), List.end());
+    vector<int> dp(n+1,0);
+    for(int i=0;i<List.size();i++){
+        int pos = List[i].second;
+        for(auto next : G[pos]){
+            if(dist[pos] < dist[next]) dp[pos] = max(dp[pos], dp[next]+1);
+        }
+    }
+
+    rep1(i,n){
+        cout << dp[i] << " ";
+    }
     return 0;
 }
