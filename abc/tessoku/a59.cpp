04a3ed3f7e7e47b10a4cfea796532edc37f54740
Hayato-T-08 <104160005+Hayato-T-08@users.noreply.github.com>
2024-06-18 17:50:04 +0900

    add

---
+++ abc/tessoku/a59.cpp
@@ -0,0 +1,107 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define rep3(i, m, n) for (int i = (int)(m); i < (int)(n); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define el endl
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define YES cout << "YES" << el
+#define NO cout << "NO" << el
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+const int inf = 1e9;
+const int mod = 1e9+7;
+const ll linf = 1e18;
+const string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string abc = "abcdefghijklmnopqrstuvwxyz";
+template <typename T>
+void printVec(const std::vector<T>& array) {
+    for (const auto& elem : array) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void print2DVec(const std::vector<std::vector<T>>& array) {
+    for (const auto& row : array) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+         }
+        std::cout << std::endl;
+    }
+}
+// 4近傍、(一般的に)上右下左
+//const int dy[4] = {-1,0,1,0};
+//const int dx[4] = {0,1,0,-1};
+// 8方向 左上, 上, 右上, 右, 右下, 下, 左下, 左
+//const int dy[8] = {-1,-1,-1,0,1,1,1,0};
+//const int dx[8] = {-1,0,1,1,1,0,-1,-1};
+using ull = unsigned long long;
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+class SegmentTree{
+    public:
+    vector<int> dat;
+    int siz = 1;
+    void init(int n){
+        siz = 1;
+        while(siz < n) siz*=2;
+        dat.resize(siz*2+1,0);
+    }
+    void update(int pos, int x){
+        pos = pos + siz - 1;
+        dat[pos] = x;
+        while(pos >= 2){
+            pos/=2;
+            dat[pos] = (dat[pos*2] + dat[pos*2+1]);
+        }
+    }
+
+    int query(int l, int r, int a, int b ,int u){
+        if( r <= a || b <= l) return 0;
+        if( l <= a && b <= r) return dat[u];
+        int m = (a + b)/2;
+        int al = query(l,r,a,m,u*2);
+        int ar = query(l,r,m,b,u*2+1);
+        return (al+ar);
+    }
+};
+int op(int a, int b){
+    return a+b;
+}
+int e(){
+    return 0;
+}
+int main() {
+    int n;
+    cin >> n;
+    int q;
+    cin >> q;
+    segtree<int,op,e> z(n);
+    z;
+    rep(i,q){
+        int t;
+        cin >> t;
+        if(t == 1){
+            int pos,x;
+            cin >> pos >> x;
+            pos--,x;
+            z.set(pos,x);
+        }else{
+            int l,r;
+            cin >> l >> r;
+            l--,r--;
+            cout << z.prod(l,r) << el;
+        }
+    }
+    return 0;
+}
---
+++ abc/tessoku/b58.cpp
@@ -0,0 +1,78 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define rep3(i, m, n) for (int i = (int)(m); i < (int)(n); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define el endl
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define YES cout << "YES" << el
+#define NO cout << "NO" << el
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+const int inf = 1e9;
+const int mod = 1e9+7;
+const ll linf = 1e18;
+const string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string abc = "abcdefghijklmnopqrstuvwxyz";
+template <typename T>
+void printVec(const std::vector<T>& array) {
+    for (const auto& elem : array) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void print2DVec(const std::vector<std::vector<T>>& array) {
+    for (const auto& row : array) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+         }
+        std::cout << std::endl;
+    }
+}
+// 4近傍、(一般的に)上右下左
+//const int dy[4] = {-1,0,1,0};
+//const int dx[4] = {0,1,0,-1};
+// 8方向 左上, 上, 右上, 右, 右下, 下, 左下, 左
+//const int dy[8] = {-1,-1,-1,0,1,1,1,0};
+//const int dx[8] = {-1,0,1,1,1,0,-1,-1};
+using ull = unsigned long long;
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+int op(int a,int b){
+    return max(a,b);
+}
+
+int e(){
+    return 0;
+}
+int main() {
+    int n,l,r;
+    cin >> n >> l >> r;
+    vector<int> x(n);
+    rep(i,n) cin >> x[i];
+
+    segtree<int ,op,e> Z(n);
+    rep(i,n) Z.set(i,x[i]);
+
+    vector<int> dp(n,inf);
+    dp[0] = 0;
+
+    rep(i,n){
+        for(int j = i+1;j<n;j++){
+            if(x[j]-x[i] >= l and x[j] - x[i] <= r){
+                chmin(dp[j],dp[i]+1);
+            }
+        }
+    }
+    cout << dp[n-1] << endl;
+    return 0;
+}
