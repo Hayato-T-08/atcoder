7b4fe584accfca7e8e6c77f4d51a9929ae4aa3b2
Hayato-T-08 <104160005+Hayato-T-08@users.noreply.github.com>
2024-07-19 23:58:13 +0900

    1

---
+++ abc/abc256/d.cpp
@@ -0,0 +1,85 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define rep3(i, m, n) for (int i = (int)(m); i < (int)(n); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define el endl
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define YES cout << "YES" << el
+#define NO cout << "NO" << el
+using ll = long long;
+using P = pair<int,int>;
+using Pll = pair<ll,ll>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+const int inf = 1073741823;
+const int mod = 1e9+7;
+const ll linf = 1LL << 60;
+const string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string abc = "abcdefghijklmnopqrstuvwxyz";
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+template <typename T>
+void pV(const std::vector<T>& array) {
+    for (const auto& elem : array) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void p2V(const std::vector<std::vector<T>>& array) {
+    for (const auto& row : array) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+         }
+        std::cout << std::endl;
+    }
+}
+// 4近傍、(一般的に)上右下左
+//const int dy[4] = {-1,0,1,0};
+//const int dx[4] = {0,1,0,-1};
+// 8方向 左上, 上, 右上, 右, 右下, 下, 左下, 左
+//const int dy[8] = {-1,-1,-1,0,1,1,1,0};
+//const int dx[8] = {-1,0,1,1,1,0,-1,-1};
+using ull = unsigned long long;
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+int main() {
+    int n;
+    cin >> n;
+    vector<P> lr(n);
+    rep(i,n) cin >> lr[i].first >> lr[i].second;
+
+    vector<P> after_lr;
+
+    sort(all(lr));
+    int l=lr[0].first,r = lr[0].second;
+    //lはソートされているので,l_i < l_i+1が保証されているが
+    //rは保証されていないので、r_i < r_i+1が保証されていない
+    //よって、r_i > r_i+1の場合は、rは更新しない
+    rep(i,n-1){
+        
+        if(r >= lr[i+1].first){
+            r = max(r,lr[i+1].second);//完全に含まれる場合
+        }else{
+            after_lr.emplace_back(l,r);
+            l = lr[i+1].first;
+            r = lr[i+1].second;
+        }
+    }
+    after_lr.emplace_back(l,r);
+    // cout << el;
+    rep(i,after_lr.size()){
+        cout << after_lr[i].first << " " << after_lr[i].second << el;
+    }
+    return 0;
+}
---
+++ abc/abc258/d.cpp
@@ -0,0 +1,76 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define rep3(i, m, n) for (int i = (int)(m); i < (int)(n); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define el endl
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
+#define YES cout << "YES" << el
+#define NO cout << "NO" << el
+using ll = long long;
+using P = pair<int,int>;
+using Pll = pair<ll,ll>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+const int inf = 1073741823;
+const int mod = 1e9+7;
+const ll linf = 1LL << 60;
+const string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string abc = "abcdefghijklmnopqrstuvwxyz";
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+    os << '(' << p.first << ", " << p.second << ')';
+    return os;
+}
+template <typename T>
+void pV(const std::vector<T>& array) {
+    for (const auto& elem : array) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void p2V(const std::vector<std::vector<T>>& array) {
+    for (const auto& row : array) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+         }
+        std::cout << std::endl;
+    }
+}
+// 4近傍、(一般的に)上右下左
+//const int dy[4] = {-1,0,1,0};
+//const int dx[4] = {0,1,0,-1};
+// 8方向 左上, 上, 右上, 右, 右下, 下, 左下, 左
+//const int dy[8] = {-1,-1,-1,0,1,1,1,0};
+//const int dx[8] = {-1,0,1,1,1,0,-1,-1};
+using ull = unsigned long long;
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+int main() {
+    int n;
+    ll x;
+    cin >> n >> x;
+    vector<ll> a(n), b(n);
+    rep(i,n) cin >> a[i] >> b[i];
+    vector<ll> sum(n+1);
+    ll ans = linf;
+    ll min_b=b[0];
+    rep(i,n){
+        if(i==0) sum[i] = a[i] + b[i];
+        else{
+            sum[i] = sum[i-1] + a[i] + b[i];
+            chmin(min_b, b[i]);
+        }
+        ll cost = (x-(i+1))*min_b + sum[i];
+        chmin(ans, cost);
+    }
+    cout << ans << endl;
+    return 0;
+}
